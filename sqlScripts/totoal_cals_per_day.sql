3f60e24ad32a859ce2d859e5e91193394ece170a
Admir <adopilot@gmail.com>
2016-10-08 14:36:14 +0200

    ado dodao nove

--- sqlScripts/totoal_cals_per_day.sql
+++ sqlScripts/totoal_cals_per_day.sql
@@ -1,6 +1,7 @@
-CREATE DEFINER=`ado`@`%` PROCEDURE `total_calls_for_period`(IN odDatuma date,IN doDatuma date)
+CREATE DEFINER=`iservis`@`localhost` PROCEDURE `totoal_cals_per_day`(IN odDatuma date,IN doDatuma date)
 BEGIN
-select 
+select
+	datum.datum as datum, 
 	COALESCE(datum.ivo,0) + COALESCE(cttq,0)  + COALESCE(aiqLT3sec,0) as tic ,/*--total incoming calls*/
     COALESCE(datum.ivo,0) + COALESCE(aiqLT3sec,0) as ivo ,/*-- IVR only is calss whit  dcontext as in clu casr + (add) calls transfert to the que and abandonada in 3 sec  --*/
     COALESCE(cttq,0) as cttq  , /*--Calls transfered to Queue--*/
@@ -9,25 +10,25 @@ select
     COALESCE(cpl.toc,0) as toc  , /*--total out gouin  calls--*/
 	COALESCE(cpl.tocc,0) as tocc  , /*--total out goin conetcted calls--*/
     cast(COALESCE(cpl.attfoc,0) as SIGNED ) attfoc  ,/*--Average Talk Time for Out Calls (sec)--*/
-    /**wrap up --*/
-	case 
+    case 
 			when COALESCE(cpl.tocc,0)=0 then CAST(0 as SIGNED) /*--ako nemamo poziva onda je wrap up time 0--*/
-			/*when date(datum.datum)<cast('2016-04-01 00:00:00' as date) then  CAST(20 as SIGNED)  --ovo je FIXN jer je ova funkcionalonst implenmetnirana tek od 1.4.2016-*/
+			when date(datum.datum)<cast('2016-04-01 00:00:00' as date) then  CAST(20 as SIGNED)  /*--ovo je FIXN jer je ova funkcionalonst implenmetnirana tek od 1.4.2016-*/ 
 			else COALESCE(totalWrapUpForOutgoingCalls,0)/COALESCE(cpl.tocc,1) /*--imamo ukupno skenudi a ovo je samo koliko nam treba ovoga--*/
-					end  as awupfoc  ,
-
-	cast(COALESCE(cpl.attfoc,0) as SIGNED ) +  /*--Average Talk Time for Out Calls (sec)--*/
+					end  as awupfoc  ,/*---- Average Wrap up for Out Calls--*/
+    cast(COALESCE(cpl.attfoc,0) as SIGNED ) +  /*--Average Talk Time for Out Calls (sec)--*/
 			case 
 					when COALESCE(cpl.tocc,0)=0 then CAST(0 as SIGNED) /*--ako nemamo poziva onda je wrap up time 0--*/
-
+					when date(datum.datum)<cast('2016-04-01 00:00:00' as date)  then  CAST(20 as SIGNED)  /*--ovo je FIXN jer je ova funkcionalonst implenmetnirana tek od 1.4.2016-*/ 
 					else COALESCE(totalWrapUpForOutgoingCalls,0)/COALESCE(cpl.tocc,1) /*--imamo ukupno skenudi a ovo je samo koliko nam treba ovoga--*/
 			end as ahtfoc  ,/*Average handling time for OUT Calls*/
-	cast(COALESCE(svi_pozivi_u_call_pro.attic,0) as SIGNED ) as attic  , /*-- Average Talk time Incoming Calls (sec)--*/		
-	case when COALESCE(aac,0)=0 then 0 /*--ako agent nije answer call onda neamoamo ni wratp up time -*/
+    cast(COALESCE(svi_pozivi_u_call_pro.attic,0) as SIGNED ) as attic  , /*-- Average Talk time Incoming Calls (sec)--*/
+    case when COALESCE(aac,0)=0 then 0 /*--ako agent nije answer call onda neamoamo ni wratp up time -*/
+		 when date(datum.datum)<cast('2016-04-01 00:00:00' as date) then  CAST(20 as SIGNED) /*--ako je datuinm prije 1.4.2016 onda je finix wrapup od 20 sec --*/
 		 else COALESCE(totalWrapUpForIncomingCalls,0)/ COALESCE(aac,0) /*--za sve ostalo dijelimo ukupno wrapup vijeme sa brojem dolaznih poziva--*/
 			end as awutic  , /*--Average Wrap up time IN Calls (sec)-*/
-	cast(COALESCE(svi_pozivi_u_call_pro.attic,0) as SIGNED )+ 
+    cast(COALESCE(svi_pozivi_u_call_pro.attic,0) as SIGNED )+ 
 			case when COALESCE(aac,0)=0 then 0 /*--ako agent nije answer call onda neamoamo ni wratp up time -*/
+			when date(datum.datum)<cast('2016-04-01 00:00:00' as date) then  CAST(20 as SIGNED) /*--ako je datuinm prije 1.4.2016 onda je finix wrapup od 20 sec --*/
 			else COALESCE(totalWrapUpForIncomingCalls,0)/ COALESCE(aac,0) /*--za sve ostalo dijelimo ukupno wrapup vijeme sa brojem dolaznih poziva--*/
 		end as ahtfic  , /*--Average handling time for IN Calls (sec)--*/
     COALESCE(COALESCE(icaw15sec,0)/COALESCE(aac,0),0)*100 as sl85  , /*--Service Level 80/15-*/
@@ -39,7 +40,7 @@ select
 	adw /*Avg Duration Wait*/
     from (
     select 
-		1 as datum,
+		date(calldate) as datum,
         suM(
 			case dcontext
 				/*-- IVR only --*/
@@ -50,7 +51,6 @@ select
 				when 'app-announcement-4' then 1
 				when 'app-announcement-5' then 1
 				when 'app-announcement-6' then 1
-				when 'app-announcement-7' then 1
 				when 'app-announcement-7' then 1
 				when 'ivr-4' then 1
 				else 0 
@@ -75,11 +75,12 @@ select
 	,sum(case when left(dstchannel,15) = 'SIP/TO_LOGOSOFT' AND left(channel,5) = 'SIP/5' and billsec>0 then 1 else 0 end)  as tocDirConCall /*--total outgiung direct calls connected --*/
 	,avg(case when left(dstchannel,15) = 'SIP/TO_LOGOSOFT' AND left(channel,5) = 'SIP/5' and billsec>0 then billsec else null end)   AS tocDirConCallAvgDur /*--average call durataion for direct calls from extensions*/
     from asteriskcdrdb.cdr c
-	where c.calldate between date( odDatuma )  and date( doDatuma )	
+    where c.calldate between date(odDatuma) and date(doDatuma)	
+    group by date(calldate)
     ) as datum
   left outer join (
 		select 
-				 1 as datum
+				 date(e.datetime_end) as datum
 				,sum(case  when e.status = 'abandonada'  and duration_wait<=3  then 0 else 1 end  ) as cttq /*--Calls transfered to Queue  exclude calls abandonada in 3 sec becouse they are in IVR-onli set  --*/
 				,sum( case  when e.status = 'abandonada'  and duration_wait<=3  then 1 else 0 end ) as aiqLT3sec  /*Abandoned in Queue in less then 3 sec then this is IVR only call-*/
 				,sum( case  when e.status = 'abandonada'  and duration_wait>3  then 1 else 0 end ) as aiq  /*Abandoned in Queue-*/
@@ -91,11 +92,12 @@ select
                 ,SUM(case when e.status='terminada' and COALESCE(duration_wait,0) < 30 then 1 else 0 end)  icaw30sec  /*-inbound calls answered within 30 sec since the connection with the bank*/
                 ,SUM(case when e.status='terminada' and COALESCE(duration_wait,0) < 15 then 1 else 0 end)  icaw15sec  /*-inbound calls answered within 30 sec since the connection with the bank*/
 				from call_center_pro.call_entry e
-				where date(e.datetime_end) between date( odDatuma )  and date( doDatuma )
+                where e.datetime_end between date(odDatuma) and date(doDatuma)	
+				group  by date(e.datetime_end)
 ) as svi_pozivi_u_call_pro on datum.datum=svi_pozivi_u_call_pro.datum  
 LEFT OUTER JOIN (
 select 
-	1 AS datum
+	DATE(datetime_entry) AS datum
 	,sum(case new_status
 				when 'Dialing' then 1
 			else 0 end) as toc /*--total outgoing calls--*/     
@@ -106,21 +108,22 @@ select
 		when 'Hangup' then duration end) as attfoc /*Average Talk Time for Out Calls (sec)*/       
 from call_center_pro.call_progress_log
 where id_call_outgoing is not null 
-and datetime_entry between date( odDatuma )  and date( doDatuma )	
-
+and   datetime_entry between date(odDatuma) and date(doDatuma)
+GROUP BY DATE(datetime_entry) 
 )    AS cpl ON datum.datum=cpl.datum
 
 left outer join (
 select 
- 1 as datum
+ date(audit.datetime_init) as datum
 ,case when id_call_outgoing is not null then sum(second(audit.duration)) end as totalWrapUpForOutgoingCalls
 ,case when id_call_incoming is not null then sum(second(audit.duration)) end as totalWrapUpForIncomingCalls
 
 from audit
-left outer join  call_progress_log on audit.id_agent=call_progress_log.id_agent and audit.datetime_init=call_progress_log.datetime_entry
+inner join call_progress_log on audit.id_agent=call_progress_log.id_agent and audit.datetime_init=call_progress_log.datetime_entry
 where id_break=9 
 and call_progress_log.duration is not null
-and datetime_entry between date( odDatuma )  and date( doDatuma )	
-) as wrapUpTimes on datum.datum=wrapUpTimes.datum;
-
+and   datetime_init between date(odDatuma) and date(doDatuma)
+group by date(audit.datetime_init)
+) as wrapUpTimes on datum.datum=wrapUpTimes.datum
+where datum.datum between date(odDatuma) and date(doDatuma)	;	
 END
